03c52a2153f181b7795483860f11900ba397e13a
Rylee Andrews <ryleeandrews73@gmail.com>
2019-05-13 08:03:30 -0600

    please

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 mongoose.Promise = require('bluebird');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true } );
+mongoose.connect('mongodb://RyleeAW:hhwwww1@ds131971.mlab.com:31971/totalscreen');
 require('./lib/server.js').start();
